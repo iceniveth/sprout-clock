d350897bc233a5b0988b206b93e7fbf905ab7577
iceniveth <ken11zer01@gmail.com>
2026-03-04 23:54:42 +0800

    Add clock out script

---
+++ clockOut.ts
@@ -0,0 +1,39 @@
+import { chromium } from "playwright";
+import { Temporal } from "@js-temporal/polyfill";
+
+process.loadEnvFile();
+
+console.log("Scraping with Playwright...");
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    args: ["--start-maximized"], // or --start-fullscreen / --kiosk
+  });
+
+  const context = await browser.newContext({ viewport: null });
+  const page = await context.newPage();
+
+  await page.goto("https://xlr8.app.sprout.ph");
+
+  await page.fill("[name='username']", process.env.APP_USERNAME || "");
+  await page.fill("[name='password']", process.env.APP_PASSWORD || "");
+
+  await page.click("[name='login']");
+
+  await page.getByRole("button", { name: "Clock Out" }).click();
+
+  await page
+    .getByRole("dialog")
+    .getByRole("button", { name: "Clock Out" })
+    .click();
+
+  // wait for 2 seconds to ensure the dialog is fully rendered
+  await page.waitForTimeout(2_000);
+
+  const timestamp = Temporal.Now.plainDateTimeISO()
+    .toString({ fractionalSecondDigits: 0 }) // drop subseconds
+    .replace(/:/g, "-");
+  const screenshotPath = `screenshots/clock-out-${timestamp}.png`;
+  await page.screenshot({ path: screenshotPath });
+})();
