ba6477474efbeefbf4fd6a13c58554d5c13d4563
iceniveth <ken11zer01@gmail.com>
2026-03-04 20:44:40 +0800

    Implement clock out

--- index.ts
+++ index.ts
@@ -25,18 +25,22 @@ console.log("Scraping with Playwright...");
   // do a page click by getByRole('button', { name: 'Clock Out' })
   await page.getByRole("button", { name: "Clock Out" }).click();
 
+  // generate timestamped filename e.g. screenshot-2026-01-02T13-15-00.png
+
+  // optionally click the button once it's visible
+  await page
+    .getByRole("dialog")
+    .getByRole("button", { name: "Clock Out" })
+    .click();
+
   // wait for 2 seconds to ensure the dialog is fully rendered
   await page.waitForTimeout(2_000);
 
-  // generate timestamped filename e.g. screenshot-2026-01-02T13-15-00.png
   const timestamp = Temporal.Now.plainDateTimeISO()
     .toString({ fractionalSecondDigits: 0 }) // drop subseconds
     .replace(/:/g, "-");
   const screenshotPath = `screenshots/screenshot-${timestamp}.png`;
   await page.screenshot({ path: screenshotPath });
-
-  // optionally click the button once it's visible
-  // await page.getByRole("dialog").getByRole("button", { name: "Clock Out" }).click();
 
   // await browser.close();
   // console.log("browser closed");
